921f7a68f6688eeb02132f311d4d74bb47ecff2a
7Dany6 <dany.sulimov@yandex.ru>
2021-10-29 12:13:51 +0300

    Solved problem "Sending geoposition to wrong person"

--- handlers.py
+++ handlers.py
@@ -2,7 +2,7 @@ import logging
 import sqlite3
 
 import requests
-from collections import deque
+from collections import defaultdict
 
 from aiogram.dispatcher import FSMContext
 from config import API_KEY, USER_ID, DB_NAME
@@ -16,7 +16,7 @@ from classes import Form, Person
 from sql import SQL
 database = SQL(f'{DB_NAME}')
 
-tracking_id = deque()
+queries = defaultdict(list)
 
 
 @dp.message_handler(commands="start")
@@ -61,14 +61,14 @@ async def registration(message: types.Message):
 @dp.message_handler(lambda message: message.text == "Register")
 async def registration(message: types.Message):
     await Form.name.set()
-    await message.answer('Was ist deine Vorname?')
+    await message.answer('What is your name?')
 
 
 @dp.message_handler(state=Form.name)
 async def process_name(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['name'] = message.text
-    await message.answer('Was ist deine Familienname?')
+    await message.answer('What is your surname?')
     await Form.next()
 
 
@@ -77,8 +77,8 @@ async def process_surname(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
         data['surname'] = message.text
         try:
-            database.register(f"@{message.from_user.username}", data['name'], data['surname'], message.from_user.id)
-            await message.answer('Vielen Dank fur die Registrierung!')
+            database.register(f"@{message.from_user.username}", data['name'], data['surname'], message.from_user.id) # contact
+            await message.answer('Thank you for the registration!')
             await bot.send_message(USER_ID,
                                    text=f"New user: {' '.join([data['name'], data['surname'], f'@{message.from_user.username}'])}")
         except sqlite3.IntegrityError:
@@ -92,36 +92,33 @@ async def track_person(message: types.Message):
 
 @dp.message_handler()
 async def find_person(message: types.Message):
-    tracking_id.append(message.from_user.id)
-    tracked_username = message.text
     try:
         assert '@' in message.text
+        queries[database.tracked_id(message.text)[0][0]].append(message.from_user.id)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+        button1 = types.KeyboardButton("Yes", request_location=True)
+        button2 = types.KeyboardButton("No", request_contact=True)
+        keyboard.add(button1, button2)
+        await bot.send_message(database.tracked_id(message.text)[0][0],
+                               text=f"User @{message.from_user.username} wants to track you, are you agree?",
+                               reply_markup=keyboard)
     except AssertionError:
         await bot.send_message(message.from_user.id, text="Please enter username WITH '@'")
-    result = database.tracked_id(tracked_username)
-    Person.tracked_id = result[0][0]
-    #print(tracking_id, Person.tracked_id)
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    button1 = types.KeyboardButton("Yes", request_location=True)
-    button2 = types.KeyboardButton("No", request_contact=True)
-    keyboard.add(button1, button2)
-    await bot.send_message(Person.tracked_id,
-                           text=f"User @{message.from_user.username} wants to track you, are you agree?",
-                           reply_markup=keyboard)
+
 
 
     @dp.message_handler(content_types=["location"])
-    async def give_position(message: types.Location):
+    async def give_position(message: types.Message):
         result = requests.get(url=f'https://geocode-maps.yandex.ru/1.x/?apikey={API_KEY}&geocode={message["location"]["longitude"]},{message["location"]["latitude"]}&format=json&lang=ru_RU')
         json_data = result.json()
-        await bot.send_message(chat_id=tracking_id[0],
+        await bot.send_message(chat_id=queries[message.from_user.id][-1],
                                text=json_data['response']['GeoObjectCollection']['featureMember'][1]['GeoObject']['metaDataProperty']['GeocoderMetaData']['text'])
-        await bot.send_message(chat_id=tracking_id[0],
+        await bot.send_message(chat_id=queries[message.from_user.id][-1],
                                text=' '.join([str(message['location']['latitude']), str(message['location']['longitude'])]))
-        tracking_id.popleft()
+        queries[message.from_user.id].pop()
 
     @dp.message_handler(content_types=["contact"])
-    async def give_contact(message: types.Contact):
-        await bot.send_message(chat_id=tracking_id[0],
+    async def give_contact(message: types.Message):
+        await bot.send_message(chat_id=queries[message.from_user.id][-1],
                                text=f"Unfortunately, your request has been rejected but you can call {message['contact']['phone_number']}")
-        tracking_id.popleft()
+        queries[message.from_user.id].pop()

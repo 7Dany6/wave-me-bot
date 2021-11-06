b93cbd55c400452277ec579cb9f1be88c6dc1845
7Dany6 <dany.sulimov@yandex.ru>
2021-11-06 16:43:21 +0300

    Changed the way buttons are shown

--- keyboard.py
+++ keyboard.py
@@ -1,4 +1,4 @@
 buttons = ["Look at last geopositions", "Feedback", "Track a person"]
-buttons_for_tracked_person = ['Yes', 'No']
-buttons_for_last_geopositions = ['Yep', 'Nope', "I am OK"]
+buttons_for_tracked_person = ['Yes', "No, I'm OK"]
+buttons_for_last_geopositions = ['Yep', "Nope, I'm OK"]
 #buttons_for_track_a_person = ["Track a geoposition", "Look at last geopositions", "Cancel"]

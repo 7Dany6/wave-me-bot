c8dc2efaebab0a1ce545032092ad6bad9591a485
7Dany6 <dany.sulimov@yandex.ru>
2022-01-27 13:03:20 +0300

    Corrected all incompatibilities

--- handlers.py
+++ handlers.py
@@ -87,40 +87,124 @@ async def cancel_handler(message: types.Message, state: FSMContext):
     logging.info('Cancelling state {}'.format(current_state))
     await cancel(message.from_user.id)
 
+@dp.message_handler(commands='instr', state="*")
+async def instruction(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state:
+        await state.finish()
+    await bot.send_message(message.from_user.id,
+                           text=_("Hi!\n\nWe all have people to care about and now you can do it not disturbing them sending a request with only one button!\n\nI'm glad to help you out\n\nWith my helping hand you can get where this ot that person is or you can get an emoji with his state.\n\nPress /start and enjoy:\n\n-/care to check location/state (through clip symbol)\n\n-/feedback to leave your opinion about bot\n\n-/sent to know how many emojis you've sent\n\n-/received to know how many emojis you've received\n\n-/add_place to add location to your favourite\n\nP.S. Use me via smartphone, not PC!"
+                           ))
+
+
+@dp.message_handler(commands="received", state="*")
+async def return_number_received_emojis(message: types.Message, state: FSMContext):
+    print('return received emojis')
+    current_state = await state.get_state()
+    if current_state:
+        await state.finish()
+    if not database.existence_received_emoji_user_received(message.from_user.id):
+        await bot.send_message(message.from_user.id,
+                               text=_("You haven't received emojis, send someone a request by clicking /care!"))
+    else:
+        await bot.send_message(message.from_user.id,
+                               text=_("You've received: \n"
+                                      "{0} {1}\n"
+                                      "{2} {3}\n"
+                                      "{4} {5}\n"
+                                      "{6} {7}\n"
+                                      "{8} {9}\n").format(database.count_received_emojis_victory(message.from_user.id)[0][0], "\u270C",
+                                                          database.count_received_emojis_snowflake(message.from_user.id)[0][0], "\u2744",
+                                                          database.count_received_emojis_cold(message.from_user.id)[0][0], "\U0001F976",
+                                                          database.count_received_emojis_fire(message.from_user.id)[0][0], "\U0001F525",
+                                                          database.count_received_emojis_snowman(message.from_user.id)[0][0], "\u2603"))
+
+
+
+@dp.message_handler(commands="sent", state="*")
+async def return_number_sent_emojis(message: types.Message, state: FSMContext):
+    print('return sent emojis')
+    current_state = await state.get_state()
+    if current_state:
+        await state.finish()
+    if not database.existence_received_emoji_user_sent(message.from_user.id):
+        await bot.send_message(message.from_user.id,
+                               text=_("You haven't sent emojis yet!"))
+    else:
+        await bot.send_message(message.from_user.id,
+                               text=_("You've sent: \n"
+                                      "{0} {1}\n"
+                                      "{2} {3}\n"
+                                      "{4} {5}\n"
+                                      "{6} {7}\n"
+                                      "{8} {9}\n").format(database.count_sent_emojis_victory(message.from_user.id)[0][0], "\u270C",
+                                                           database.count_sent_emojis_snowflake(message.from_user.id)[0][0], "\u2744",
+                                                           database.count_sent_emojis_cold(message.from_user.id)[0][0], "\U0001F976",
+                                                           database.count_sent_emojis_fire(message.from_user.id)[0][0], "\U0001F525",
+                                                          database.count_sent_emojis_snowman(message.from_user.id)[0][0], "\u2603"))
+
+
+
+@dp.message_handler(commands="feedback", state='*')
+async def feedback(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    print(current_state)
+    if current_state:
+        await state.finish()
+    await bot.send_message(message.from_user.id, text=_("Leave your opinion, it will improve the bot!"))
+    await Form.feedback.set()
+    current_state = await state.get_state()
+    print(current_state)
+
+
+    @dp.message_handler(content_types=['text'], state=Form.feedback)
+    async def process_feedback(message: types.Message, state: FSMContext):
+        print("comment")
+        await bot.send_message(USER_ID,
+                               text=f"Feedback from [{message.from_user.first_name}](tg://user?id={message.from_user.id}): {message.text}",
+                               parse_mode=ParseMode.MARKDOWN_V2)
+        await state.finish()
+
+
 @dp.message_handler(commands='add_place', state="*")
-async def add_location(message: types.Message):
+async def add_location(message: types.Message, state: FSMContext):
+    current_state = await state.get_state()
+    if current_state:
+        await state.finish()
     await bot.send_message(message.from_user.id, text=_("Please, enter the name of a location!"))
     await Form.enter_location.set()
 
 
-@dp.message_handler(content_types=['text'], state=Form.enter_location)
-async def name_location(message: types.Message):
-    location_names[message.from_user.id].append(message.text.encode('utf-8'))
-    print(location_names)
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    button = types.KeyboardButton(_("Share a location"), request_location=True)
-    keyboard.add(button)
-    await bot.send_message(message.from_user.id,
-                           text=_("Please, <b>switch on your location</b> and press the button!"),
-                           parse_mode=ParseMode.HTML,
-                           reply_markup=keyboard)
-    await Form.fav_location.set()
+    @dp.message_handler(content_types=['text'], state=Form.enter_location)
+    async def name_location(message: types.Message):
+        location_names[message.from_user.id].append(message.text.encode('utf-8'))
+        print(location_names)
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+        button = types.KeyboardButton(_("Share a location"), request_location=True)
+        keyboard.add(button)
+        await bot.send_message(message.from_user.id,
+                               text=_("Please, <b>switch on your location</b> and press the button!"),
+                               parse_mode=ParseMode.HTML,
+                               reply_markup=keyboard)
+        #await Form.fav_location.set()
 
 
-@dp.message_handler(content_types=['location'], state=Form.fav_location)
+@dp.message_handler(content_types=['location'], state="*")
 async def send_fav_location(message: types.Message, state: FSMContext):
+    print(location_names)
     if not location_names[message.from_user.id]:
-        await give_position(message=message, state="*")
         print('relocate')
+        await give_position(message=message, state=Form.enter_location)
     database.add_location_name(message.from_user.id, location_names[message.from_user.id].pop().decode('utf-8'),
                                message['location']['longitude'], message['location']['latitude'])
     await bot.send_message(message.from_user.id, text=_("Location has been registered!"))
     print(database.coordinates(message.from_user.id))
-    await state.finish()
+    #await state.finish()
 
 
 @dp.message_handler(commands="care", state='*')
 async def track_person(message: types.Message, state: FSMContext):
+    print('care')
     if database.tracking_existance(message.from_user.id):
         keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
         buttons_for_tracking = [database.get_first_name(contact[0])[0][0] for contact in
@@ -217,11 +301,12 @@ async def give_position(message: types.Message, state: FSMContext):
     queries[message.from_user.id].pop()
         #last_geopositions[message.from_user.id].append(f"{json_data['response']['GeoObjectCollection']['featureMember'][1]['GeoObject']['metaDataProperty']['GeocoderMetaData']['text']}")  # база с координатами, временем, contact и кто просил
     await check_queries(queries, message.from_user.id)
-    await state.finish()
 
 
-@dp.message_handler(content_types=["text"], state="*")
+
+@dp.message_handler(content_types=["text"], state=Form.send_geo)
 async def send_emoji(message: types.Message):
+    print(message.text)
     if not database.existence_received_emoji(queries[message.from_user.id][-1], message.from_user.id):
         if message.text[:-1] == _("It's alright"):
                 database.add_to_received_emoji_if_victory(queries[message.from_user.id][-1], message.from_user.id)
@@ -276,74 +361,4 @@ async def send_emoji(message: types.Message):
                                database.get_name(queries[message.from_user.id][-1])[0][0],
                                queries[message.from_user.id][-1],
                                database.get_contact(queries[message.from_user.id][-1])[0][0])
-
-
-@dp.message_handler(commands='instr', state="*")
-async def instruction(message: types.Message, state: FSMContext):
-    await bot.send_message(message.from_user.id,
-                           text=_("Hi!\n\nWe all have people to care about and now you can do it not disturbing them sending a request with only one button!\n\nI'm glad to help you out\n\nWith my helping hand you can get where this ot that person is or you can get an emoji with his state.\n\nPress /start and enjoy:\n\n-/care to check location/state (through clip symbol)\n\n-/feedback to leave your opinion about bot\n\n-/sent to know how many emojis you've sent\n\n-/received to know how many emojis you've received\n\n-/add_place to add location to your favourite\n\nP.S. Use me via smartphone, not PC!"
-                           ))
-
-
-@dp.message_handler(commands="received", state="*")
-async def return_number_received_emojis(message: types.Message):
-    print('return received emojis')
-    if not database.existence_received_emoji_user_received(message.from_user.id):
-        await bot.send_message(message.from_user.id,
-                               text=_("You haven't received emojis, send someone a request by clicking /care!"))
-    else:
-        await bot.send_message(message.from_user.id,
-                               text=_("You've received: \n"
-                                      "{0} {1}\n"
-                                      "{2} {3}\n"
-                                      "{4} {5}\n"
-                                      "{6} {7}\n"
-                                      "{8} {9}\n").format(database.count_received_emojis_victory(message.from_user.id)[0][0], "\u270C",
-                                                          database.count_received_emojis_snowflake(message.from_user.id)[0][0], "\u2744",
-                                                          database.count_received_emojis_cold(message.from_user.id)[0][0], "\U0001F976",
-                                                          database.count_received_emojis_fire(message.from_user.id)[0][0], "\U0001F525",
-                                                          database.count_received_emojis_snowman(message.from_user.id)[0][0], "\u2603"))
-
-
-
-@dp.message_handler(commands="sent", state="*")
-async def return_number_sent_emojis(message: types.Message):
-    print('return sent emojis')
-    if not database.existence_received_emoji_user_sent(message.from_user.id):
-        await bot.send_message(message.from_user.id,
-                               text=_("You haven't sent emojis yet!"))
-    else:
-        await bot.send_message(message.from_user.id,
-                               text=_("You've sent: \n"
-                                      "{0} {1}\n"
-                                      "{2} {3}\n"
-                                      "{4} {5}\n"
-                                      "{6} {7}\n"
-                                      "{8} {9}\n").format(database.count_sent_emojis_victory(message.from_user.id)[0][0], "\u270C",
-                                                           database.count_sent_emojis_snowflake(message.from_user.id)[0][0], "\u2744",
-                                                           database.count_sent_emojis_cold(message.from_user.id)[0][0], "\U0001F976",
-                                                           database.count_sent_emojis_fire(message.from_user.id)[0][0], "\U0001F525",
-                                                          database.count_sent_emojis_snowman(message.from_user.id)[0][0], "\u2603"))
-
-
-
-@dp.message_handler(commands="feedback", state='*')
-async def feedback(message: types.Message, state: FSMContext):
-    current_state = await state.get_state()
-    print(current_state)
-    if current_state:
-        await state.finish()
-    await bot.send_message(message.from_user.id, text=_("Leave your opinion, it will improve the bot!"))
-    await Form.feedback.set()
-    current_state = await state.get_state()
-    print(current_state)
-
-
-@dp.message_handler(content_types=['text'], state=Form.feedback)
-async def process_feedback(message: types.Message, state: FSMContext):
-    print("comment")
-    await bot.send_message(USER_ID,
-                           text=f"Feedback from [{message.from_user.first_name}](tg://user?id={message.from_user.id}): {message.text}",
-                           parse_mode=ParseMode.MARKDOWN_V2)
-    await state.finish()
 
